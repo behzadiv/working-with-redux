58e98a0a33cb46e8b88485a5fd34e8877d162c91
Behzad <saeedibehzad@gmail.com>
2021-11-18 16:55:55 +0330

    using CakeContainer + wrap Provider of react-redux in App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-
-import './App.css';
-
+import "./App.css";
+import CakeContainer from "./components/CakeContainer";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <CakeContainer />
+      </div>
+    </Provider>
   );
 }
 
